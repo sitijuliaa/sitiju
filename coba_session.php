5745a00f509af7014df9c3a0f5bc53adf2e19147
sitijuliaa <110010453+sitijuliaa@users.noreply.github.com>
2022-08-31 11:24:17 +0700

    Add files via upload

---
+++ coba_session.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta username="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="set_session.php" method="post">
+        <p>username <input name="username" type="text"></p>
+        <p>id <input name="id" type="number"></p>
+
+        <button type="submit">proses</nutton>
+        
+</body>
+</html>
